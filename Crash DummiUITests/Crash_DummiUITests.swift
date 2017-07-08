f3c931ff7eb9b96785beb93fdbee89268b247f68
JedidiahBertram <jedidiah.bertram@gmail.com>
2017-07-08 16:27:09 -0500

    Initial Commit

---
+++ Crash DummiUITests/Crash_DummiUITests.swift
@@ -0,0 +1,36 @@
+//
+//  Crash_DummiUITests.swift
+//  Crash DummiUITests
+//
+//  Created by Jedidiah Bertram on 7/8/17.
+//  Copyright © 2017 Jedidiah Bertram. All rights reserved.
+//
+
+import XCTest
+
+class Crash_DummiUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
